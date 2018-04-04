97bf434fed0587e34500aa5d69c34f8877e6c277
area51agent <matzapper@yahoo.com>
2018-04-04 14:30:59 -0400

    Corrected layout of Submit and logout buttons.

--- desktop.php
+++ desktop.php
@@ -53,11 +53,11 @@
                     </select>
                 </li>
 
-                <li><button data-toggle="modal" data-target="#Add_Modal" class="btn btn-block" id="addBtn">Add Item</button></li>
-                <li><button class="btn btn-danger btn-block" onclick="window.location.href='logout.php'">Logout</button></li>
+                <li style="margin-left: 25px;"><button data-toggle="modal" data-target="#Add_Modal" class="btn btn-block" id="addBtn">Add Item</button></li>
+                <li style="margin-left: 50px;"><button class="btn btn-success btn-block" onclick="submitFinal()">Submit Transfer</button></li>
               </ul>
               <ul class="nav navbar-nav navbar-right">
-                <li style="margin-right: 50px;"><button class="btn btn-success btn-block" onclick="submitFinal()">Submit Transfer</button></li>
+                <li style="margin-right: 50px;"><button class="btn btn-danger btn-block" onclick="window.location.href='logout.php'">Logout</button></li>
               </ul>
           </div>
         </nav>
