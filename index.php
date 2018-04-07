27bcaadbbfa73cb6521fd38d7fe057eb05dc88e1
Jon Knight <jmknight2@pstcc.edu>
2018-04-07 01:37:11 -0400

    Fixed small, but catastrophic syntax error...

--- index.php
+++ index.php
@@ -171,42 +171,42 @@
             </div>
         </div>
         <?php
-            if($_SESSION['auth'] == true)
-            {
-              $script = <<<EOT
-              <script>
-              var isMobile = {
-              Android: function() {
-                return navigator.userAgent.match(/Android/i);
-              },
-              BlackBerry: function() {
-                return navigator.userAgent.match(/BlackBerry/i);
-              },
-              iOS: function() {
-                return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-              },
-              Opera: function() {
-                return navigator.userAgent.match(/Opera Mini/i);
-              },
-              Windows: function() {
-                return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
-              },
-              any: function() {
-                return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-              }
-              };
-              if (isMobile.any()) {
-                document.location.replace('mobile.php');
-              }
-              else {
-                document.location.replace('desktop.php');
-              }
-              </script>
-              </body>
-              </html>
-            EOT;
-                echo $script;
-            }
+if($_SESSION['auth'] == true)
+{
+  $script = <<<EOT
+  <script>
+  var isMobile = {
+  Android: function() {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function() {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function() {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function() {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function() {
+    return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+  },
+  any: function() {
+    return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+  }
+  };
+  if (isMobile.any()) {
+    document.location.replace('mobile.php');
+  }
+  else {
+    document.location.replace('desktop.php');
+  }
+  </script>
+  </body>
+  </html>
+EOT;
+    echo $script;
+}
             ?>
     </body>
 </html>
