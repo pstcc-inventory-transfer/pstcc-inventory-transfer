0c066a88b9eaec0dd5220e80bcf56a329447bbfa
zachMitchell <zdmitchellsd@gmail.com>
2018-04-12 17:05:13 -0400

    Today was a wild adventure; as such I have an aftermath of files that need to properly be merged with web_ui before they're ready. right now it's sortof a mix of files that may or may not work :P

--- index.php
+++ index.php
@@ -1,174 +1,301 @@
 <?php
     session_start();
     $_SESSION['auth'] = false;
-?>
-
+    ?>
 <!doctype html>
-
 <html>
-
-<head>
-    <title>PSTCC Transfer</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-    <style>
-    html {
-      height: 100%;
-      width: 100%;
-    }
-    body
-    {
-      background: radial-gradient(rgb(255, 210, 79), rgb(0, 75, 141));
-      background-size: cover;
-      color: white;
-      display: flex;
-      width: 100%;
-    }
-    .container-fluid {
-      width: 100%;
-    }
-    form
-    {
-        margin-top: 20px;
-    }
-    .col-centered
-    {
-        margin: 0 auto;
-        float: none;
-    }
-    #login-panel
-    {
-      margin: 0 auto;
-      margin-top: 14%;
-      text-align: center;
-      background-color: #0066cc;
-      padding: 10px 60px 20px 60px;
-      border-radius: 30px;
-      border: 2px solid #fcd955;
-      width: 60%;
-    }
-    @media screen and (max-width: 768px) {
-      [class*="col-"] {
-        width: 75%;
-      }
-      body {
-        text-align: center;
-      }
-      #login-panel
-      {
-        margin: 0 auto;
-        margin-top: 25%;
-        text-align: center;
-        background-color: #0066cc;
-        padding: 10px 30px 20px 30px;
-        border-radius: 20px 20px 20px 20px;
-        border: 2px solid #fcd955;
-        width: 90%;
-        height: 75%;
-        display: block;
-        float: none;
-      }
-      h2 {
-        font-size: 20px;
-      }
-    }
-    </style>
-
-</head>
-
-<body>
-
-    <div class="container-fluid">
-    <div class="row">
-      <div class="col-sm-8 col-centered" id="login-panel">
-
-          <img src="img_assets/pelli_full.svg"/>
-          <h1>Transfer Application</h1>
-
-              <?php
-                if (isset($_POST["submit"]))
+    <head>
+        <title>PSTCC Transfer</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        
+        <style>
+            html {
+            height: 100%;
+            width: 100%;
+            }
+            body
+            {
+            background: radial-gradient(rgb(255, 210, 79), rgb(0, 75, 141));
+            background-size: cover;
+            color: white;
+            display: flex;
+            width: 100%;
+            }
+            a
+            {
+            cursor: pointer;
+            }
+            .container-fluid {
+            width: 100%;
+            }
+            .vertical-center {
+            min-height: 100%;  /* Fallback for browsers do NOT support vh unit */
+            min-height: 100vh; /* These two lines are counted as one :-)       */
+            display: flex;
+            align-items: center;
+            }
+            form
+            {
+            margin-top: 20px;
+            }
+            .col-centered
+            {
+            margin: 0 auto;
+            float: none;
+            }
+            
+            .modal-header
+            {
+                padding:9px 15px;
+                border-bottom:1px solid #eee;
+                -webkit-border-top-left-radius: 5px;
+                -webkit-border-top-right-radius: 5px;
+                -moz-border-radius-topleft: 5px;
+                -moz-border-radius-topright: 5px;
+                 border-top-left-radius: 5px;
+                 border-top-right-radius: 5px;
+             }
+            
+            .reset-header
+            {
+                color: white;
+                background-color: #33b5e5;
+            }
+            
+            .alert-header
+            {
+                color: white;
+                background-color: #ff4444;
+            }
+            
+            #alertModal > *
+            {
+                width: 400px;
+                margin: auto;
+                margin-top: 30px;
+            }
+            
+            #login-panel
+            {
+            margin: 0 auto;
+            text-align: center;
+            background-color: #0066cc;
+            padding: 10px 60px 20px 60px;
+            border-radius: 30px;
+            border: 2px solid #fcd955;
+            width: 50%;
+            }
+            @media screen and (max-width: 768px) {
+                body {
+                text-align: center;
+                }
+                #login-panel
                 {
-                  //Due to the password being passed as an excutable argument, it needs to be checked for spaces so nothing can go haywire on the other side:
-                  $passHasSpaces = false;
-                  for($i=0;$i<strlen($_POST['pwd']);$i++){
-                    if($_POST['pwd'][$i] == ' '){
-                      $passHasSpaces = true;
-                      break;
-                    }
-                  }
-                    if($_POST["pwd"] == '')
+                margin: 0 auto;
+                text-align: center;
+                background-color: #0066cc;
+                padding: 10px 30px 20px 30px;
+                border-radius: 20px 20px 20px 20px;
+                border: 2px solid #fcd955;
+                width: 90%;
+                height: 75%;
+                display: block;
+                float: none;
+                }
+                h2 {
+                font-size: 20px;
+                }
+            }
+        </style>
+        
+        <script>
+            function alertModal(title, body)
+            {
+                $('#alert-modal-title').text(title);
+                $('#alert-modal-body').text(body);
+                
+                $('#alertModal').modal('show');
+            }
+            
+            function testReset(username)
+            {
+                $.ajax(
+                {
+                    method: "POST",
+                    url: "phpFunctions.php",
+                    data:
                     {
-                        $_SESSION['auth'] = false;
-                        echo "<script>alert('Password cannot be blank.');</script>";
+                        user: username
                     }
-                    else if ($passHasSpaces){
-                      $_SESSION['auth'] = false;
-                      echo "<script>alert('Password cannot contain spaces.');</script>";
-                    }
-                    else if(exec('cd verification && scramblerVerify.exe '.$_POST["pwd"].' -f nothingInteresting.txt') == 'True')
+                }).done(function(results)
+                {
+                   console.log(results); 
+                });
+            }
+            
+            function validatePwd()
+            {
+                var password = $('#pwd').val();
+                var username = $('#user').val();
+                console.log(username);
+                if(username != null)
+                {
+                    if(password !== '')
                     {
-                        $_SESSION['auth'] = true;
+                        if(!password.includes(' '))
+                        {
+                            $.ajax(
+                            {
+                                method: "POST",
+                                url: "validatePwd.php",
+                                data:
+                                {
+                                    user: username,
+                                    pwd: password
+                                }
+                            }).done(function(results)
+                            {
+                                if(results == 'tech true')
+                                {
+                                    var isMobile = {
+                                    Android: function() {
+                                    return navigator.userAgent.match(/Android/i);
+                                    },
+                                    BlackBerry: function() {
+                                    return navigator.userAgent.match(/BlackBerry/i);
+                                    },
+                                    iOS: function() {
+                                    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+                                    },
+                                    Opera: function() {
+                                    return navigator.userAgent.match(/Opera Mini/i);
+                                    },
+                                    Windows: function() {
+                                    return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+                                    },
+                                    any: function() {
+                                    return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+                                    }
+                                    };
+                                    if (isMobile.any()) {
+                                    document.location.replace('mobile.php');
+                                    }
+                                    else {
+                                    document.location.replace('desktop.php');
+                                    }
+                                }
+                                else if(results == 'admin true')
+                                {
+                                    window.location.href = 'inventoryScan.php';
+                                }
+                                else
+                                {
+                                    alertModal('Error', results + 'Incorrect password');
+                                }
+                            });
+                        }
+                        else
+                        {
+                            alertModal('Error', 'Password cannot include spaces.');
+                        }
                     }
                     else
                     {
-                        $_SESSION['auth'] = false;
-                        echo "<script>alert('Password incorrect.');</script>";
+                        alertModal('Error', 'Password cannot be blank.');
                     }
                 }
-              ?>
-
-          <form method="post">
-            <div class="form-group">
-                <div class="input-group">
-                    <input class="form-control" style="width: 100%" name="pwd" id="pwd" placeholder="Technician Password" type="password">
-                    <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+                else
+                {
+                    alertModal('Error', 'Please select a user.');
+                }
+            }
+        </script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row vertical-center">
+                <div class="col-sm-8 col-centered" id="login-panel">
+                    <img src="img_assets/pelli_full.svg"/>
+                    <h1>Transfer Application</h1>
+                    <hr/>
+                    <div class="container-fluid" style="margin: 0; padding: 0;" method="post">
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-6">
+                                <div class="form-group" style="text-align: left; width: 100%;">
+                                    <select class="form-control" style="width: 100%;" id="user" name="user">
+                                        <option value="null" selected disabled>Please select user</option>
+                                        <option value="admin">Administrator</option>
+                                        <option value="tech">Technician</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-6">
+                                <div class="form-group" style="text-align:left; width: 100%;">
+                                    <div class="input-group" style="width: 100%;">
+                                        <input class="form-control" style="width: 100%" name="pwd" id="pwd" placeholder="Password" type="password">
+                                        <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <button name="submit" class="btn btn-info btn-block" style="margin-top: 15px;" onclick="validatePwd();">Submit</button>
+                            </div>
+                        </div>
+                        <br/>
+                        <br/>
+                        <a style="color: white; margin-top: 10px;" data-target="#resetModal" data-toggle="modal">Forgot your password?</a>
+                    </div>
                 </div>
             </div>
-            <button type="submit" name="submit" class="btn btn-info btn-block" style="margin-top: 15px;">Submit</button>
-          </form>
-
-      </div>
-
-    </div>
-    <?php
-      if($_SESSION['auth'] == true)
-      {
-        $script = <<<EOT
+        </div>
+        <div id="resetModal" class="modal fade" role="dialog" style="color: black;">
+            <div class="modal-dialog">
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header reset-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Password Reset</h4>
+                    </div>
+                    <div class="modal-body" style="text-align: right">
+                        <h5 style="text-align: left;"><b>Select password to be reset:</b></h5>
+                        <select class="form-control" style="width: 100%;" id="resetUser" name="resetUser">
+                            <option value="admin">Administrator</option>
+                            <option value="tech">Technician</option>
+                        </select>
+                        <h5 style="text-align: left;">Upon confirmation, a reset link will be sent to <b>[insert email here].</b></h5>
+                        <br/>
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                        <button type="button" class="btn btn-success" data-dismiss="modal" onclick="testReset($('#resetUser').val());">Confirm</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div id="alertModal" class="modal fade" role="dialog" style="color: black; text-align: left;">
+            <div class="modal-dialog">
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header alert-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title" id="alert-modal-title"></h4>
+                    </div>
+                    <div class="modal-body" style="text-align: center;">
+                        <p id="alert-modal-body"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
         <script>
-        var isMobile = {
-        Android: function() {
-          return navigator.userAgent.match(/Android/i);
-        },
-        BlackBerry: function() {
-          return navigator.userAgent.match(/BlackBerry/i);
-        },
-        iOS: function() {
-          return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-        },
-        Opera: function() {
-          return navigator.userAgent.match(/Opera Mini/i);
-        },
-        Windows: function() {
-          return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
-        },
-        any: function() {
-          return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-        }
-        };
-        if (isMobile.any()) {
-          document.location.replace('mobile.php');
-        }
-        else {
-          document.location.replace('desktop.php');
-        }
-        </script>
-        </body>
-        </html>
-EOT;
-          echo $script;
-      }
-    ?>
+            //This is nice :)
+        pwd.onkeydown = (function(event){
+                if(event.keyCode == 13)
+                    validatePwd();
+            });</script>
+    </body>
+</html>
---
+++ resetPassword.php
@@ -0,0 +1,210 @@
+<?php
+    include 'db_connection.php';
+    date_default_timezone_set('UTC');
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <title>PSTCC Transfer</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        
+        <style>
+            html {
+            height: 100%;
+            width: 100%;
+            }
+            body
+            {
+            background: radial-gradient(rgb(255, 210, 79), rgb(0, 75, 141));
+            background-size: cover;
+            color: white;
+            display: flex;
+            width: 100%;
+            }
+
+            .container-fluid {
+            width: 100%;
+            }
+            .vertical-center {
+            min-height: 100%;  /* Fallback for browsers do NOT support vh unit */
+            min-height: 100vh; /* These two lines are counted as one :-)       */
+            display: flex;
+            align-items: center;
+            }
+            .col-centered
+            {
+            margin: 0 auto;
+            float: none;
+            }
+            
+            .modal-header
+            {
+                padding:9px 15px;
+                border-bottom:1px solid #eee;
+                -webkit-border-top-left-radius: 5px;
+                -webkit-border-top-right-radius: 5px;
+                -moz-border-radius-topleft: 5px;
+                -moz-border-radius-topright: 5px;
+                 border-top-left-radius: 5px;
+                 border-top-right-radius: 5px;
+             }
+            
+            .alert-header
+            {
+                color: white;
+                background-color: #ff4444;
+            }
+            
+            #alertModal > *
+            {
+                width: 400px;
+                margin: auto;
+                margin-top: 30px;
+            }
+            
+            #login-panel
+            {
+            margin: 0 auto;
+            text-align: center;
+            background-color: #0066cc;
+            padding: 10px 60px 20px 60px;
+            border-radius: 20px;
+            border: 2px solid #fcd955;
+            width: 50%;
+            }
+            @media screen and (max-width: 768px) {
+                body {
+                text-align: center;
+                }
+                #login-panel
+                {
+                    width: 90%;
+                }
+            }
+        </style>
+        
+        <script>
+            function alertModal(title, body)
+            {
+                $('#alert-modal-title').text(title);
+                $('#alert-modal-body').text(body);
+                
+                $('#alertModal').modal('show');
+            }
+            
+            function updatePwd(username, password)
+            {
+                if(password !== "")
+                {
+                    if(!password.includes(' '))
+                    {
+                        $.ajax(
+                            {
+                                method: "POST",
+                                url: "phpFunctions.php",
+                                data:
+                                {
+                                    user: username,
+                                    newPwd: password
+                                }
+                            }).done(function(results)
+                            {
+                                if(results.trim() == "true")
+                                {
+                                    alertModal('Succes', 'Password successfully changed!');
+                                }
+                                else
+                                {
+                                    alertModal('Error', results);
+                                }
+                            });
+                    }
+                    else
+                    {
+                        alertModal('Error', 'Password cannot include spaces.');
+                    }
+                }
+                else
+                {
+                    alertModal('Error', 'Password cannot be blank.');
+                }
+            }
+        </script>
+    </head>
+    <body>
+<?php
+
+    if(isset($_GET['q1']) && isset($_GET['q2']))
+    {
+        $pwdDateHash = urldecode($_GET['q1']);
+        $user = $_GET['q2'];
+        
+        $today = date('m-d');
+        
+        $query = "SELECT password FROM tblUsers WHERE userName = '".($user == 'admin'?'Administrator':'Technician')."'";
+        $result = queryDB(connectToDB(), $query);
+        $pwdDB = trim($result[0]['password']);
+
+        if(exec('.\verification\scramblerVerify.exe -d "'.$pwdDB.$today.'" "'.$pwdDateHash.'"') == 'True')
+        {
+            ?>
+                <div class="container-fluid">
+                    <div class="row vertical-center">
+                        <div class="col-sm-8 col-centered" id="login-panel">
+                            <h1>Password Reset</h1>
+                            <hr/>
+                            <div class="container-fluid" style="margin: 0; padding: 0;" method="post">
+                                <div class="row">
+                                    <div class="col-xs-12">
+                                        <div class="form-group" style="text-align: left; width: 100%;">
+                                            <h5><b>New Password:</b></h5>
+                                            <input class="form-control" type="password" id="newPwd" name="newPwd">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-xs-12">
+                                        <button name="submit" class="btn btn-info btn-block" style="margin-top: 15px;" onclick="updatePwd( '<?php echo $user; ?>', $('#newPwd').val());">Submit</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            <?php
+        }
+        else
+        {
+            echo "It doesn't appear this link is valid. Please ensure you've receieved this email today.";
+            //echo "<br>\"".exec('.\verification\scramblerVerify.exe -e "'.$pwdDB.$today.'"')."\"<br>\"".$pwdDateHash.'"<br>'.exec('.\verification\scramblerVerify.exe -d "'.$pwdDB.$today.'" "z?£DOgOb¿r¿`U9nc|aillk¢cSdueh`[r~b{"').'<br>'.$today;
+            //exec('echo "'.$pwdDateHash.'" > .\verification\test.txt');
+        }
+    }
+    else
+    {
+        header('Location: index.php');
+    }
+?>
+        
+        <div id="alertModal" class="modal fade" role="dialog" style="color: black; text-align: left;">
+            <div class="modal-dialog">
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header alert-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title" id="alert-modal-title"></h4>
+                    </div>
+                    <div class="modal-body" style="text-align: center;">
+                        <p id="alert-modal-body"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
